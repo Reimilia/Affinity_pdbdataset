26538e9f6efff2251d36a5e1caa6689d1293241c
wy <panzer.wy@gmail.com>
2016-10-31 11:57:54 -0400

    update skeleton of each class

---
+++ affinity_pdbdataset/utility/PDB_container.py
@@ -0,0 +1,46 @@
+
+
+class PDB_container:
+    '''
+    Use to read a pdb file from a cocrystal structure and parse
+     it for further use
+     Note this class cannot write any files and we do not want any
+     files to be added by this class
+     This is good for IO management although the code might be hard to
+    understand
+    '''
+
+    def __init__(self):
+        '''
+        parse a pdb from file
+        '''
+        pass
+
+    def register_a_ligand(self):
+        pass
+
+    def Is_pure_protein(self):
+        pass
+
+    def Is_pure_nucleic(self):
+        pass
+
+    def Is_protein_nucleic_complex(self):
+        pass
+
+    def Bundle_ligand_result_dict(self):
+        pass
+
+    def Bundle_ligand_result_list(self):
+        pass
+
+    def list_ligand_ResId(self):
+        pass
+
+    def get_ligand_dict(self):
+        pass
+
+
+class ligand_container:
+    pass
+
--- affinity_pdbdataset/utility/file_utility/atomgroup_filepack.py
+++ affinity_pdbdataset/utility/file_utility/atomgroup_filepack.py
@@ -5,6 +5,12 @@ import commands
 import gzip
 
 class atomgroup_filepack:
+    '''
+    All files need to be put in one single folder
+    But it can share one single folder from other filepacks
+    This is because autodock vina needs its receptor and ligand
+    in the same folder
+    '''
 
     instance_index = 0
 
@@ -12,7 +18,7 @@ class atomgroup_filepack:
     related_filelist={}
 
     # This is critical for setting the output file direction
-    dump_filedir_prefix=''
+    __dump_filedir_prefix=''
 
     def __init__(self):
         pass
@@ -43,6 +49,9 @@ class atomgroup_filepack:
         '''
         pass
 
+    def get_autodock_receptor(self):
+        pass
+
     def prepare_autodock_receptor(self,repair_policy=None):
         '''
 
@@ -50,12 +59,17 @@ class atomgroup_filepack:
         '''
         pass
 
+    def get_autodock_ligand(self):
+        pass
 
     def prepare_autodock_ligand(self,repair_policy=None):
         '''
 
         :return:
         '''
+        pass
+
+    def get_output_filename(self,format):
         pass
 
     def prepare_output_file(self,format,repair_policy=None):
@@ -66,6 +80,11 @@ class atomgroup_filepack:
         '''
         pass
 
-
     def import_atomgroup_from_file(self,filename,repair_policy=None):
+        pass
+
+    def import_atomgroup_from_parser(self,parse):
+        pass
+
+    def repair_atomgroup(self):
         pass
